431d4a4b8a66bf3c203f4f8bdba4a3704e5e0b11
Stanislav Yurchenko <stanislavyurchenko@Stanislavs-MacBook-Pro.local>
2021-08-30 23:31:03 +0300

    added add-edit-todo form

---
+++ apps/todos/src/app/modules/add-edit-form/add-edit-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'learning-workspace-add-edit-form',
+  templateUrl: './add-edit-form.component.html',
+  styleUrls: ['./add-edit-form.component.scss']
+})
+export class AddEditFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ apps/todos/src/app/modules/add-edit-form/add-edit-form.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddEditFormComponent } from './add-edit-form.component';
+
+
+
+@NgModule({
+  declarations: [
+    AddEditFormComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AddEditFormModule { }
